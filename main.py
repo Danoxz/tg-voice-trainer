c2dee7a4c1f814d1e733fa3a0bb100b5b1f35903
jeanenep <jeanenep@student.21-school.ru>
2024-08-03 01:57:59 +0700

    2

--- main.py
+++ main.py
@@ -25,17 +25,17 @@ async def handle_message(event):
     sender = await event.get_sender()
     sender_id = sender.id
 
-    if sender_id in whitelist:
-        logger.info(f"Message from whitelisted user {sender_id}. Skipping.")
-        return
-
-    # Получение имени отправителя
+    # Get sender name
     if hasattr(sender, 'username') and sender.username:
         sender_name = f"@{sender.username}"
     elif hasattr(sender, 'first_name') and sender.first_name:
         sender_name = sender.first_name
     else:
         sender_name = "Unknown"
+
+    if sender_id in whitelist:
+        logger.info(f"Message from whitelisted user {sender_name}. Skipping.")
+        return
     
     message_time = event.message.date.strftime("%Y-%m-%d %H:%M:%S")
 
@@ -47,14 +47,13 @@ async def handle_message(event):
 
     logger.info(f"Short voice received from {sender_name} ({sender_id}) at {message_time}. Duration: {voice_duration} seconds")
     
-    # Delete voice message
-    try:
-        await event.message.delete()
-        logger.info(f"Deleted voice from {sender_name} ({sender_id}) at {message_time}")
-        
-        # Reply
+    # Delete and reply
+    try:        
         await event.reply("Это автоответчик\nПривет! Может стоит написать текстом?")
         logger.info(f"Replied to voice from {sender_name} ({sender_id}) at {message_time}")
+
+        await event.message.delete()
+        logger.info(f"Deleted voice from {sender_name} ({sender_id}) at {message_time}")
     except Exception as e:
         logger.error(f"Error deleting or replying to voice message from {sender_name} ({sender_id}) at {message_time}: {e}")
 
